6949ec7804458424d850c58d537780fc48f57f12
Rafael Cardoso <rafael_eng31@outlook.com>
2023-02-17 09:15:43 -0300

    Criar o projeto da API

---
+++ services/SchoolRecords/src/SRD.API/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SRD.API.Controllers
+{
+    public class UserController : ControllerBase
+    {
+        public IActionResult GetUsers()
+        {
+            return Ok();
+        }
+    }
+}
