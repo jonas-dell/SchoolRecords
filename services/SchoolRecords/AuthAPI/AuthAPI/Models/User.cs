c9ee99f95b283b862e49933a231eee89a4bf95c5
Adam <adam.machado12@gmail.com>
2023-02-23 17:36:51 -0300

    ADD: API de Autenticação de usario

---
+++ services/SchoolRecords/AuthAPI/AuthAPI/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthAPI.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Token { get; set; }
+        public string Role { get; set; }
+        public string Email { get; set; }
+    }
+}
