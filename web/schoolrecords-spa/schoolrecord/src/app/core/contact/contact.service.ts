f315dad0cb0c191cee4043e474afc8a275c6b9d5
Rafael Cardoso <rafael_eng31@outlook.com>
2023-10-28 13:39:22 -0300

    Adicionar um contato

--- web/schoolrecords-spa/schoolrecord/src/app/core/contact/contact.service.ts
+++ web/schoolrecords-spa/schoolrecord/src/app/core/contact/contact.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { delay } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 import { CurrentUserService } from 'src/app/shared/services/current-user.service';
 import contacts from '../../data/contacts.json';
 import { ConfigService } from '../config/config.services';
@@ -18,14 +17,7 @@ export class ContactService {
     this.contactsDb = contacts;
   }
 
-  getContacts({ pageSize, page }) {
-    let skip = (page - 1) * pageSize;
-    let take = page * pageSize;
-    let contactsFiltered = this.contactsDb.slice(skip, take);
-    return of(contactsFiltered).pipe(delay(1000));
-  }
-
-  getContactsTest({ pageSize, page }): Observable<any> {
+  getContacts({ pageSize, page }): Observable<any> {
     let skip = (page - 1) * pageSize;
     let take = page * pageSize;
     let user = this.currentUserService.getCurrentUser();
