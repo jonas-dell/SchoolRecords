ae08af30782e70b7a1423bcd45ea21594a18aaf9
Adam <adam.machado12@gmail.com>
2023-11-01 00:32:57 -0300

    Add: InserPost In front-end

--- web/schoolrecords-spa/schoolrecord/src/app/publish-post/publish-post.component.ts
+++ web/schoolrecords-spa/schoolrecord/src/app/publish-post/publish-post.component.ts
@@ -1,19 +1,41 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { BaseFormComponent } from '../shared/base-form/base-form.component';
+import { NotificationService } from '../shared/services/notification.service';
+import { PostService } from './publish-post.services';
 
 @Component({
   selector: 'app-publish-post',
   templateUrl: './publish-post.component.html',
   styleUrls: ['./publish-post.component.css']
 })
-export class PublishPostComponent implements OnInit {
 
+
+export class PublishPostComponent extends BaseFormComponent implements OnInit {
+  formulario = new FormGroup({
+    Id: new FormControl(0, [Validators.nullValidator]),
+    Name: new FormControl('', [Validators.nullValidator]),
+    Image: new FormControl('', [Validators.nullValidator]),
+    Post: new FormControl('', [Validators.nullValidator]),
+    Date: new FormControl('', [Validators.nullValidator]),
+    PerfilId: new FormControl(0, [Validators.nullValidator]),
+    
+
+  });
+
+  
   constructor(
+    private postService: PostService,
+    private notificationService: NotificationService,
     public dialogRef: MatDialogRef<PublishPostComponent>,
-    @Inject(MAT_DIALOG_DATA) public data
-  ) { }
+    @Inject(MAT_DIALOG_DATA) public data) 
+   { 
+    super(dialogRef);
+   }
 
   ngOnInit(): void {
+
   }
 
   closeDialog() {
@@ -25,7 +47,9 @@ export class PublishPostComponent implements OnInit {
   }
 
   save() {
-    alert('Salvar alguma coisa');
+    this.postService.postInsert(this.formulario.value).subscribe(()=>{
+      this.notificationService.success("Salvar alguma coisa!");
+      this.closeDialog();
+    })
   }
-
 }
